768ace71222ac7af76d0c7f53ecbb0e93d9e888e
andrew <0x9171@gmail.com>
2020-09-16 00:32:39 +0300

    upd

--- bitset.go
+++ bitset.go
@@ -2,23 +2,26 @@ package bitset
 
 import (
 	"encoding/binary"
-	"errors"
+	"sync"
 )
 
-var ErrOutOfRange = errors.New(`bit number out of range`)
-
 type BitSet struct {
-	set  []uint64
+	set  []bits
 	size uint64
+}
+
+type bits struct {
+	bits uint64
+	sync.Mutex
 }
 
 func New(size uint64) *BitSet {
 	if size%64 != 0 {
-		panic(`invalid size`)
+		panic(`bitset: invalid size`)
 	}
 
 	return &BitSet{
-		set:  make([]uint64, size/64),
+		set:  make([]bits, size/64),
 		size: size,
 	}
 }
@@ -28,48 +31,48 @@ func (bs *BitSet) Size() uint64 {
 }
 
 func (bs *BitSet) Reset() {
-	bs.set = make([]uint64, bs.size/64)
+	bs.set = make([]bits, bs.size/64)
 }
 
-func (bs *BitSet) Unmarshal(raw []byte) {
-	bs.size = binary.BigEndian.Uint64(raw[0:])
+func (bs *BitSet) UnmarshalBinary(raw []byte) error {
+	bs.size = binary.BigEndian.Uint64(raw)
 	bs.Reset()
 
 	for n := 0; uint64(n) < bs.size/64; n++ {
-		bs.set[n] = binary.BigEndian.Uint64(raw[(n*8)+8:])
+		bs.set[n] = bits{bits: binary.BigEndian.Uint64(raw[(n*8)+8:])}
 	}
 }
 
-func (bs *BitSet) Marshal() []byte {
+func (bs *BitSet) MarshalBinary() ([]byte, error) {
 	raw := make([]byte, (len(bs.set)*8)+8)
 
-	binary.BigEndian.PutUint64(raw[0:], bs.size)
+	binary.BigEndian.PutUint64(raw, bs.size)
 
-	for n, bv := range bs.set {
-		binary.BigEndian.PutUint64(raw[(n*8)+8:], bv)
+	for n, s := range bs.set {
+		binary.BigEndian.PutUint64(raw[(n*8)+8:], s.bits)
 	}
 
-	return raw
+	return raw, nil
 }
 
 func (bs *BitSet) Test(bitNum uint64) bool {
-	i := bitNum / 64
-	b := bitNum % 64
-	bv := uint64(1 << (b - 1))
+	v := uint64(1 << ((bitNum % 64) - 1))
 
-	return bs.set[i]&bv == bv
+	bs.set[bitNum/64].Lock()
+	r := bs.set[bitNum/64].bits&v == v
+	bs.set[bitNum/64].Unlock()
+
+	return r
 }
 
 func (bs *BitSet) Set(bitNum uint64) {
-	i := bitNum / 64
-	b := bitNum % 64
-
-	bs.set[i] |= 1 << (b - 1)
+	bs.set[bitNum/64].Lock()
+	bs.set[bitNum/64].bits |= 1 << ((bitNum % 64) - 1)
+	bs.set[bitNum/64].Unlock()
 }
 
 func (bs *BitSet) Unset(bitNum uint64) {
-	i := bitNum / 64
-	b := bitNum % 64
-
-	bs.set[i] &= ^(1 << (b - 1))
+	bs.set[bitNum/64].Lock()
+	bs.set[bitNum/64].bits &= ^(1 << ((bitNum % 64) - 1))
+	bs.set[bitNum/64].Unlock()
 }
